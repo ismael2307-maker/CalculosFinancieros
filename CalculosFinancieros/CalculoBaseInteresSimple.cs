978e0f536c6d72767098fa813e91aa9445a8ab65
Ismael <142625821+ismael2307-maker@users.noreply.github.com>
2024-10-23 19:19:36 -0600

    Avanzado interes de interes simple (Agregada la interface y clase para el calculo base)

---
+++ CalculosFinancieros/CalculoBaseInteresSimple.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculosFinancieros
+{
+    public abstract class CalculoBaseInteresSimple : ICalculoInteresSimple
+    {
+        protected double ValorPresente;
+        protected double ValorFuturo;
+        protected double Periodo;
+        protected double Interes;
+
+        public CalculoBaseInteresSimple(double valorPresente, double valorFuturo,double periodo) 
+        {
+            ValorPresente = valorPresente;
+            ValorFuturo = valorFuturo;
+            Periodo = periodo;
+        }
+
+        public abstract void CalcularInteresSimple();
+       
+
+        public object ObtenerResultadoFormateado()
+        {
+            string formattedPresente = ValorPresente.ToString("N0");
+            string formattedFuturo = ValorFuturo.ToString("N0");
+
+            return new
+            {
+                Presente = "C$ " + formattedPresente,
+                Interes = Interes,
+                Periodo = Periodo,
+                Futuro = "C$ "+formattedFuturo
+            };
+        }
+    }
+}
---
+++ CalculosFinancieros/ICalculoInteresSimple.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculosFinancieros
+{
+    public interface ICalculoInteresSimple
+    {
+        void CalcularInteresSimple();
+        object ObtenerResultadoFormateado();
+    }
+}
